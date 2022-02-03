85a62ebf1471849bc4a6e4c18aa55ae9dd079735
Shashikant Dangi <u20cs072@coed.svnit.ac.in>
2022-02-03 13:44:56 +0530

    added dynamic routes and pages

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const fs = require('fs')
 const path = require('path')
+const uuid = require('uuid')
 
 const app = express()
 
@@ -10,8 +11,19 @@ app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-    const pathname = path.join(__dirname, 'views', 'index.html')
-    res.sendFile(pathname)
+    const pathname = path.join(__dirname, 'user.json')
+    const data = fs.readFileSync(pathname)
+    const datainjson = JSON.parse(data)
+    res.render('index', {'users': datainjson})
+})
+
+app.post('/', function (req, res) {
+    const submittedData = req.body
+    // const pathname = path.join(__dirname, 'user.json')
+    // const data = fs.readFileSync(pathname)
+    // const datainjson = JSON.parse(data)
+    
+    res.redirect (`/${submittedData.username}/profile`)
 })
 
 app.get('/contact', function(req, res){
@@ -29,16 +41,63 @@ app.post('/contact', function(req, res){
     res.redirect('/')
 })
 
-app.get('/profile', function (req, res) {
-    res.render('profile')
+app.get('/:id/profile', function (req, res) {
+    const userid = req.params.id
+    const pathname = path.join(__dirname, 'user.json')
+    const data = fs.readFileSync(pathname)
+    const datainjson = JSON.parse(data)
+    for (user of datainjson){
+        if (user.id === userid){
+            return res.render('profile', {'user': user})
+        }
+    }
+    res.redirect('/')
 })
 
-app.get('/grades', function (req, res) {
-    res.render('grades')
+app.get('/admin', function(req, res){
+    const pathname = path.join(__dirname, 'views', 'admin.html')
+    res.sendFile(pathname)
 })
 
-app.get('/schedule', function (req, res) {
-    res.render('schedule')
+app.post('/admin', function(req, res){
+    const data = req.body
+    data.id = uuid.v4();
+    const pathname = path.join(__dirname, 'user.json')
+    const already = fs.readFileSync(pathname)
+    const datainjson = JSON.parse(already)
+    datainjson.push(data)
+    fs.writeFileSync(pathname, JSON.stringify(datainjson))
+    res.redirect('/')
+})
+
+app.get('/:id/grades', function (req, res) {
+    const userid = req.params.id
+    const pathname = path.join(__dirname, 'user.json')
+    const data = fs.readFileSync(pathname)
+    const datainjson = JSON.parse(data)
+    for (user of datainjson){
+        if (user.id === userid){
+            return res.render('grades', {'user': user})
+        }
+    }
+    res.redirect('/')
+})
+
+app.get('/:id/schedule', function (req, res) {
+    const userid = req.params.id
+    const pathname = path.join(__dirname, 'user.json')
+    const data = fs.readFileSync(pathname)
+    const datainjson = JSON.parse(data)
+    for (user of datainjson){
+        if (user.id === userid){
+            return res.render('schedule', {'user': user})
+        }
+    }
+    res.redirect('/')
+})
+
+app.use(function(req, res){
+    res.redirect('/')
 })
 
 app.listen(3000)
